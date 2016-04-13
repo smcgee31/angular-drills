e07dcb9e7b9846d05b9c6054aa6ea60fd92a9658
smcgee31 <solodesign31@gmail.com>
2016-04-12 20:01:39 -0600

    catchup

--- app4-api/service.js
+++ app4-api/service.js
@@ -1,15 +1,35 @@
-angular.module('apiApp').service('mainSvc', function($http) {
+angular.module('apiApp').service('mainSvc', function($http, $q) {
 
+// ---- Example of $http shorthand -----
 
     this.getPokemon = function(choose) {
-        return $http({
-            method: 'GET',
-            url: 'http://pokeapi.co/api/v2/pokemon/' + choose + '/'
-        }).then(function(response) {
-            console.log(response.data);
-            return response.data;
+        var deferred = $q.defer();
+        $http.get('http://pokeapi.co/api/v2/pokemon/' + choose + '/')
+        .then(function(response) {
+            deferred.resolve(response.data);
         });
+        return deferred.promise;
     };
+
+
+
+
+//  --- Example from another project  ---------
+
+    // this.getUsers = function() {
+    //     var anyNameThatRepresentsDeffered = $q.defer();
+    //     $http({
+    //         method: 'GET',
+    //         url: 'http://reqres.in/api/users?page=1'
+    //     }).then(function(response){
+    //         var parsedResponse = response.data.data;
+    //         for (var i = 0; i < parsedResponse.length; i++) {
+    //             parsedResponse[i].first_name = 'Bad-Mo-Jamma';
+    //         }
+    //         anyNameThatRepresentsDeffered.resolve(parsedResponse);
+    //     });
+    //     return anyNameThatRepresentsDeffered.promise;
+    // };
 
 
 
